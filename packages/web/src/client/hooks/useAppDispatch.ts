94b8549e5a5588a35bc49052d009ee2419231c1c
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-01-20 14:38:50 +0000

    chore: sort out deps

--- packages/web/src/client/hooks/useAppDispatch.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch } from 'react-redux'
-
-import { AppDispatch } from 'store/createStore'
-
-// Export a hook that can be reused to resolve types
-export const useAppDispatch = () => useDispatch<AppDispatch>()
--- packages/web/src/client/references/locale/index.ts
+++
@@ -1,5 +0,0 @@
-import * as enGb from './en-GB/index.json'
-
-export default {
-  en: enGb,
-}
