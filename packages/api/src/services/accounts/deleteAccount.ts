25b500c0650fe37a37e463412b1e265763ddc887
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-02-11 17:19:53 +0000

    feat: cancel (delete) sessions connected to deleting user

--- packages/api/src/services/accounts/deleteAccount.ts
+++ packages/api/src/services/accounts/deleteAccount.ts
@@ -1,4 +1,60 @@
+import { captureException, Scope } from '@sentry/node'
 import { FieldResolver } from 'nexus'
 
+import { Logger } from '../../helpers/console'
+
 export const deleteAccount: FieldResolver<'Mutation', 'userDeleteAccount'> =
-  () => null
+  async (_, _args, ctx) => {
+    const {
+      user: { userId },
+      prisma,
+    } = ctx
+
+    if (!userId) {
+      throw new Error('User must be logged in')
+    }
+
+    try {
+      /**
+       * Get sessions for user
+       */
+      const { sessions } = (await prisma.user.findUnique({
+        where: {
+          userId,
+        },
+        include: {
+          sessions: true,
+        },
+      }))!
+
+      /**
+       * Delete sessions for user
+       */
+      await prisma.session.deleteMany({
+        where: {
+          pairerId: userId,
+        },
+      })
+
+      return {
+        success: true,
+      }
+    } catch (err) {
+      const errMsg = 'Failed to delete user account'
+      Logger.error(errMsg, err)
+
+      /**
+       * Any other error, capture it & return a failed object
+       */
+      captureException(
+        errMsg,
+        new Scope().setExtras({
+          err,
+          userId,
+        })
+      )
+      return {
+        success: false,
+      }
+    }
+  }
