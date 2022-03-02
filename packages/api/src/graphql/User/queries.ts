75c65aba66bd98e9763726ce47dbe687908cc1c8
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-03-02 15:10:12 +0000

    feat: check if email is blacklisted

--- packages/api/src/graphql/User/queries.ts
+++ packages/api/src/graphql/User/queries.ts
@@ -2,24 +2,43 @@ import { extendType, nonNull, stringArg } from 'nexus'
 
 import { prisma } from '../../db/prisma'
 
+import { getSanityClientRead } from '../../lib/sanity'
+// import { getDocument } from '../../services/sanity/getDocument'
+
 export const queries = extendType({
   type: 'Query',
   definition: (t) => {
     t.field('userIsEmailUnique', {
       type: 'Boolean',
       description:
-        'Check if the email address submitted by a user or persepctive user has already been used before',
+        'Check if the email address submitted by a user or persepctive user has already been used before or is blacklisted',
       args: {
         email: nonNull(stringArg()),
       },
       resolve: async (_, { email }) => {
-        const user = await prisma.user.findFirst({
+        const prismaUser = await prisma.user.findFirst({
           where: {
             email,
           },
         })
 
-        return user === null
+        const sanityClient = getSanityClientRead()
+
+        const query = /* groq */ `*[_type == 'blacklistedEmails' && email == $email]{email}`
+
+        const sanityUser = await sanityClient.fetch(query, {
+          email: email,
+        })
+        
+        // const sanityUser = await getDocument('blacklistedEmails')
+
+        // log
+        console.log(
+          '🍐🍐🍐🍐🍐🍐🍐🍐🍐🍐🍐🍐🍐🍐🍐🍐🍐🍐🍐🍐 sanityUser is:',
+          sanityUser
+        )
+
+        return prismaUser === null && sanityUser.length === 0
       },
     })
   },
