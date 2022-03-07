aa6ec2f8d7d2b3e1db7178a77febfe8b26fd5355
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-03-07 09:56:14 +0000

    fix: approveProfile action breaks when no document exists

--- packages/cms/documentActions/approveProfile.js
+++ packages/cms/documentActions/approveProfile.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useDocumentOperation } from '@sanity/react-hooks'
 import { PAIRER_PROFILE_STATUS } from '@pairup/shared'
+import { CheckCircle } from 'phosphor-react'
 
 export const ApproveProfile = (props) => {
   const { patch, publish } = useDocumentOperation(props.id, props.type)
@@ -15,8 +16,15 @@ export const ApproveProfile = (props) => {
   }, [isPublishing, props.draft])
 
   return {
-    disabled: publish.disabled || !document.hasVerifiedAccount,
-    label: isPublishing ? 'Publishing Profile' : 'Approve Profile',
+    disabled: publish.disabled || (document && !document.hasVerifiedAccount),
+    label: isPublishing ? 'Publishing Profile...' : 'Approve Profile',
+    icon: CheckCircle,
+    title:
+      document && !document.hasVerifiedAccount
+        ? 'A user must verify their account before it can be approved'
+        : !props.draft
+        ? `This profile has all it's changes approved`
+        : '',
     onHandle: () => {
       // This will update the button text
       setIsPublishing(true)
