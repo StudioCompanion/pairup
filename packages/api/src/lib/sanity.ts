6cfc52878db35ff1d938087772033453a077bc3c
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-03-07 12:16:36 +0000

    chore: add action debug log

--- packages/api/src/lib/sanity.ts
+++ packages/api/src/lib/sanity.ts
@@ -9,11 +9,18 @@ export const getSanityClientWrite = () =>
     useCdn: false,
   })
 
-export const getSanityClientRead = () =>
-  client({
+export const getSanityClientRead = () => {
+  console.log('DEBUG >>>>>>>', {
+    dataset: process.env.SANITY_DATASET,
+    projectId: process.env.SANITY_PROJECT_ID,
+    token: process.env.SANITY_API_KEY,
+  })
+
+  return client({
     apiVersion: '2022-01-25',
     dataset: process.env.SANITY_DATASET,
     projectId: process.env.SANITY_PROJECT_ID,
     token: process.env.SANITY_API_KEY, // admin token has write access
     useCdn: true,
   })
+}
