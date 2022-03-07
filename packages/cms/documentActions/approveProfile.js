e0f4b18033e56dd14f4801466f9b9d9e28a2c856
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-03-07 11:55:55 +0000

    feat: add confirm if a profile has been rejected

--- packages/cms/documentActions/approveProfile.js
+++ packages/cms/documentActions/approveProfile.js
@@ -4,6 +4,7 @@ import { PAIRER_PROFILE_STATUS } from '@pairup/shared'
 import { CheckCircle } from 'phosphor-react'
 
 export const ApproveProfile = (props) => {
+  const [popoverVisisble, setPopoverVisible] = useState(false)
   const { patch, publish } = useDocumentOperation(props.id, props.type)
   const [isPublishing, setIsPublishing] = useState(false)
 
@@ -15,27 +16,55 @@ export const ApproveProfile = (props) => {
     }
   }, [isPublishing, props.draft])
 
+  const profileIsRejected =
+    document && document.status === PAIRER_PROFILE_STATUS.REJECTED
+
+  const approveDocument = () => {
+    // This will update the button text
+    setIsPublishing(true)
+
+    patch.execute([{ set: { status: PAIRER_PROFILE_STATUS.APPROVED } }])
+
+    // Perform the publish
+    publish.execute()
+
+    // Signal that the action is completed
+    props.onComplete()
+  }
+
+  const handleHandle = () => {
+    if (profileIsRejected) {
+      setPopoverVisible(true)
+      return
+    }
+
+    approveDocument()
+  }
+
+  const handleConfirm = () => {
+    approveDocument()
+  }
+
   return {
     disabled: publish.disabled || (document && !document.hasVerifiedAccount),
     label: isPublishing ? 'Publishing Profile...' : 'Approve Profile',
     icon: CheckCircle,
+    dialog:
+      profileIsRejected && popoverVisisble
+        ? {
+            type: 'confirm',
+            onCancel: props.onComplete,
+            message:
+              'This profile has previously been rejected, are you sure you want to approve it?',
+            onConfim: handleConfirm,
+          }
+        : null,
     title:
       document && !document.hasVerifiedAccount
         ? 'A user must verify their account before it can be approved'
         : !props.draft
         ? `This profile has all it's changes approved`
         : '',
-    onHandle: () => {
-      // This will update the button text
-      setIsPublishing(true)
-
-      patch.execute([{ set: { status: PAIRER_PROFILE_STATUS.APPROVED } }])
-
-      // Perform the publish
-      publish.execute()
-
-      // Signal that the action is completed
-      props.onComplete()
-    },
+    onHandle: handleHandle,
   }
 }
